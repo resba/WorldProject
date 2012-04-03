db4e7c103a9954b186d1eac2b57fa2c49b04efac
Matthew Sowden <zxa243@gmail.com>
2012-04-03 19:32:22 -0400

    README Text and other shenanigans

--- src/com/resbah/WorldProject/WorldProject.java
+++ src/com/resbah/WorldProject/WorldProject.java
@@ -7,20 +7,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
 
-/**
- * Created by IntelliJ IDEA.
- * User: resba
- * Date: 4/3/12
- * Time: 7:05 PM
- * To change this template use File | Settings | File Templates.
- */
 public class WorldProject extends JavaPlugin{
 
     Logger log = this.getServer().getLogger();
     
     public void onEnable(){
         log.info(this.getDescription().getName() + " Version: "+this.getDescription().getVersion()+" has been loaded.");
-        
     }
     
     public void onDisable(){
